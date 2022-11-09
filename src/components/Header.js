cffd1106c733bb3db75f8e732feda0cd2f65e4c5
AlexSheshko <007alex03@gmail.com>
2022-11-10 01:27:57 +0300

    header done

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="block row">
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/5309/5309779.png"
+        alt="Brand icon"
+      ></img>
+      <h1>My Shop</h1>
+    </header>
+  );
+}
---
+++ src/components/Menu.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Menu() {
+  return (
+    <main className="block col-2">
+        Menu
+    </main>
+  )
+}
