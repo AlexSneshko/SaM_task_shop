28a3edd9d2cb1c46d6be37c6d00b98f44756b320
AlexSheshko <007alex03@gmail.com>
2022-11-10 14:43:49 +0300

    design fixed

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,8 @@
 import CartItem from "./CartItem";
 
 export default function Cart(props) {
-  const { cartItems, onAdd, onRemove, onClear, onCleanCart, onCheckout } = props;
+  const { cartItems, onAdd, onRemove, onClear, onCleanCart, onCheckout } =
+    props;
   const totalPrice = cartItems.reduce(
     (sum, cur) => sum + cur.price * cur.quantity,
     0
@@ -10,24 +11,33 @@ export default function Cart(props) {
   return (
     <div className="block col-1 cart">
       <div className="row">
-        <h1>Cart</h1> <button onClick={() => onCleanCart()}>Clear</button>
+        <h1>Cart</h1>{" "}
+        <button onClick={() => onCleanCart()}>
+          <i class="gg-trash"></i>
+        </button>
       </div>
-      {cartItems.map((product) => (
-        <CartItem
-          key={product.id}
-          product={product}
-          onAdd={onAdd}
-          onRemove={onRemove}
-          onClear={onClear}
-        />
-      ))}
-      <div className="cart-bottom">
-        <div className="row">
-          <h2>Total:</h2>
-          <h2>{totalPrice.toFixed(2)}</h2>
+      {cartItems.length === 0 ? (
+        <p>Your cart is empty</p>
+      ) : (
+        <div>
+          {cartItems.map((product) => (
+            <CartItem
+              key={product.id}
+              product={product}
+              onAdd={onAdd}
+              onRemove={onRemove}
+              onClear={onClear}
+            />
+          ))}
+          <div className="cart-bottom">
+            <div className="row">
+              <h2>Total:</h2>
+              <h2>{totalPrice.toFixed(2)}</h2>
+            </div>
+            <button onClick={() => onCheckout()}><h2>Chekout</h2></button>
+          </div>
         </div>
-        <button onClick={() => onCheckout()}>Checkout</button>
-      </div>
+      )}
     </div>
   );
 }
--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ const data = {
   products: [
     {
       id: "1",
-      name: "Manckbook1",
+      name: "Kilua",
       price: 70.99,
       image:
         "https://i.pinimg.com/originals/c7/a0/4a/c7a04a031ed8341798792dde36ff47e2.png",
@@ -11,28 +11,38 @@ const data = {
     },
     {
       id: "2",
-      name: "Manckbook2",
-      price: 23.10,
+      name: "Jujutsu Kaisen Satoru",
+      price: 23.1,
       image:
-        "https://content2.onliner.by/catalog/device/header/1bd5893ab33b00756688fb91f9da14e8.jpeg",
+        "https://i.pinimg.com/564x/ad/8d/2b/ad8d2ba752bf0e9d7b12a38e5cf45524.jpg",
+
       description:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae magni, quae maxime officiis, modi a soluta eligendi voluptatum, culpa saepe alias. Harum possimus delectus voluptate!",
     },
     {
       id: "3",
-      name: "Manckbook3",
-      price: 14.00,
+      name: "Ayanokoji",
+      price: 14.0,
       image:
-        "https://i.pinimg.com/originals/c7/a0/4a/c7a04a031ed8341798792dde36ff47e2.png",
+        "https://i.pinimg.com/564x/3b/62/7c/3b627c1906b8cb1bf2d27566d6fa00b2.jpg",
       description:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae magni, quae maxime officiis, modi a soluta eligendi voluptatum, culpa saepe alias. Harum possimus delectus voluptate!",
     },
     {
       id: "4",
-      name: "Manckbook4",
-      price: 89.50,
+      name: "Bubblegum",
+      price: 89.5,
       image:
-        "https://i.pinimg.com/originals/c7/a0/4a/c7a04a031ed8341798792dde36ff47e2.png",
+        "https://i.pinimg.com/736x/1d/6c/da/1d6cdad95556fd1222e701f309abdad3.jpg",
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae magni, quae maxime officiis, modi a soluta eligendi voluptatum, culpa saepe alias. Harum possimus delectus voluptate!",
+    },
+    {
+      id: "5",
+      name: "Pepsi",
+      price: 74.99,
+      image:
+        "https://i.pinimg.com/564x/a6/fb/93/a6fb93a4a7bf6930b25242f550dd775e.jpg",
       description:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae magni, quae maxime officiis, modi a soluta eligendi voluptatum, culpa saepe alias. Harum possimus delectus voluptate!",
     },
